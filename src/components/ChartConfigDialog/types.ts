7b0bb5b6901faff9a01e6723ef033b9f06e94cb3
xtyuerx <xiangtaiyua06299@biaoguoworks.com>
2025-10-30 11:29:37 +0800

    fix: 配置注释

--- src/components/ChartConfigDialog/types.ts
+++ src/components/ChartConfigDialog/types.ts
@@ -34,41 +34,47 @@ export interface Props {
 
 export type ChartDataItem = Record<string, unknown>
 export interface ChartConfig {
-  id?: string
-  title?: string
+  id?: string // 图表id
+  title?: string // 图表标题
   type: ChartMainType
-  subType: ChartSubType
+  subType: ChartSubType // 图表子类型
+  /* 主题 */
   theme?: {
-    name: string
-    colors: string[]
+    name: string // 主题名称
+    colors: string[] // 主题颜色
   }
-  xField: string
-  categorySort?: 'asc' | 'desc'
-  valueFields: string[]
-  categoryField?: string
-  dataSource: number
+  xField: string // 横轴字段
+  categorySort?: 'asc' | 'desc' // 分类排序
+  valueFields: string[] // 值字段
+  categoryField?: string // 分类字段
+  dataSource: number // 数据源
+  /* 图例 */
   legend: {
-    show: boolean
-    position: LegendComponent['position']
+    show: boolean // 是否显示
+    position: LegendComponent['position'] // 图例位置
   }
+  /* 标签 */
   label: {
-    show: boolean
-    position: LabelComponent
+    show: boolean // 是否显示
+    position: LabelComponent // 标签位置
   }
+  /* 纵轴 */
   yAxis: {
-    show: boolean
-    title?: string
-    min?: number
-    max?: number
-    tickCount?: number
-    interval?: number
+    show: boolean // 是否显示
+    title?: string // 纵轴标题
+    min?: number // 纵轴最小值
+    max?: number // 纵轴最大值
+    tickCount?: number // 纵轴刻度
+    interval?: number // 纵轴间隔
   }
+  /* 横轴 */
   xAxis: {
-    show: boolean
-    title?: string
+    show: boolean // 是否显示
+    title?: string // 横轴标题
   }
+  /* 网格 */
   grid: {
-    show: boolean
+    show: boolean // 是否显示
   }
 }
 
